3a3d6669e586f4a1fad65096af19340b22bef70d
ashik-08 <ashikurrahman.8october@gmail.com>
2023-12-21 23:49:11 +0600

    added basic route

---
+++ src/pages/HomePage/HomePage.jsx
@@ -0,0 +1,10 @@
+
+const HomePage = () => {
+    return (
+        <section>
+            
+        </section>
+    );
+};
+
+export default HomePage;
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,11 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    fontFamily: {
+      play: ["Playfair Display", "serif"],
+      roboto: ["Roboto", "sans-serif"],
+    },
+    extend: {
+      colors: {
+        special: "#F7931E",
+        head: "#61C0BF",
+        "sub-head": "#333333",
+        details: "#888888",
+      },
+    },
   },
-  plugins: [],
-}
+  plugins: [require("daisyui")],
+};
